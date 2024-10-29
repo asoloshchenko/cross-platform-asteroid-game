63657a865ae0e28c6affba5f54bdff2064dca0ed
steblivets_i <steblivec1000@gmail.com>
2024-10-29 16:24:11 +0300

    fix: headers

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3909177810"
+    serviceWorkerVersion: "219072918"
   }
 });
